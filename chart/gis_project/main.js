e66ffd3f3dbfca77f6fb4d7e7f9a794468f55931
Nadezda Chikurova <nadezdachikurova@Nadezdas-MacBook-Air.local>
2021-04-28 15:05:55 -0400

    scatter plot added

--- chart/gis_project/main.js
+++ chart/gis_project/main.js
@@ -5,18 +5,15 @@ let height = 300;
 let margin = { top: 60, bottom: 50, left: 40, right: 40 };
 
 let svg;
-let slider;
-let gTime;
+let projection;
+let path;
+let div;
+let bubble;
 
 let state = {
     geojson: null,
     taxes: null,
-    selectedYear: null
-    // hover: {
-    //     latitude: null,
-    //     longitude: null,
-    //     state: null,
-    // },
+
 };
 
 /**
@@ -32,204 +29,166 @@ Promise.all([
     console.log("state: ", state);
     init();
 });
-
-
+//const formatValue = d3.format(",d")
+//const newData = new Map(state.taxes.map(d => [d.LocationDesc, d.Data_Value]))
+const formatNumber = d3.format(",.2f");
+function textTween(text, valueof) {
+    if (typeof valueof !== "function") {
+        const value = +valueof;
+        valueof = () => value;
+    }
+    return text.textTween(function (d) {
+        const value = +this.textContent;//.replace(/,/g, "");
+        const i = d3.interpolateNumber(value, valueof(d));
+        return t => formatNumber(i(t));
+    });
+}
 function init() {
 
-    // const dataTime = d3.range(0, 50).map(function (d) {
-    //     return new Date(1970 + d, 10, 3);
-    // });
-    // console.log("dataTime", dataTime)
 
-    // slider = d3
-    //     .sliderHorizontal()
-    //     .min(d3.min(dataTime))
-    //     .max(d3.max(dataTime))
-    //     .step(1000 * 60 * 60 * 24 * 365)
-    //     .width(innerWidth)
-    //     .tickFormat(d3.timeFormat('%Y'))
-    //     //.ticks(6)
-    //     .tickValues(dataTime)
-    //     .default(new Date(1990, 10, 3))
-    //     .on('onchange', val => {
-    //         d3.select('p#value-time').text(d3.timeFormat('%Y')(val));
-    //     });
-
-    // gTime = d3
-    //     .select('div#slider-time')
-    //     .append('svg')
-    //     .attr('width', 500)
-    //     .attr('height', 100)
-    //     .append('g')
-    //     .attr('transform', 'translate(30,30)')
-    //     .call(slider);
-
-    // d3.select('p#value-time').text(d3.timeFormat('%Y')(slider.value()));
-
-
-
-    const projection = d3.geoAlbersUsa().fitSize([width, height], state.geojson);
-    const path = d3.geoPath().projection(projection);
+    projection = d3.geoAlbersUsa().fitSize([width, height], state.geojson);
+    path = d3.geoPath().projection(projection);
 
     svg = d3
         .select("#map-container")
         .append("svg")
-        .attr("viewBox", "0 0 800 620")
+        .attr("viewBox", "0 0 500 320")
         .append("g")
         .attr("transform", "translate(0,0)")
-
-
-    const palette = [
-        '#fff7ea',
-        '#fcd0b1',
-        '#faa580',
-        '#f77552',
-        '#f03528',
-        '#c51625',
-        '#96071e',
-        '#680114',
-        '#3d0000'
-    ];
-
-    const colorScale = d3.scaleLinear()
-        //.range(["#e7eff0", "#C8E1E5", "#B7D0D0", "#82C0CC", "#458A93", "#16697A", "#1C474D", "#0e2629"])//"#1C474D"])
-        .domain([0, d3.max(state.taxes, d => d.Data_Value)])
-        .range(["white", "#171730"])
-    //console.log("colorDomain", colorScale.domain())
+        .attr("text-anchor", "middle")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
 
     //formatTime = d3.format(",") //if value interpreted by number
-    const formatNumber = d3.format(",.2f");
 
-    const div = d3.select("body").append("div")
+    //const formatValue = d3.format(",d")
+
+    div = d3.select("body").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
 
-    const newData = new Map(state.taxes.map(d => [d.LocationDesc, d.Data_Value]))
-    const newDataYear = new Map(state.taxes.map(d => [d.LocationDesc, d.Year]))
+    //const newData = new Map(state.taxes.map(d => [d.LocationDesc, d.Data_Value]))
+    //const newDataYear = new Map(state.taxes.map(d => [d.LocationDesc, d.Year]))
 
-    // UI ELEMENT SETUP
-    const selectElement = d3.select("#dropdown").on("change", function () {
-        console.log("new selected year  is", this.value);
-        // `this` === the selectElement
-        // this.value holds the dropdown value a user just selected
-        state.selectedYear = this.value;
-        draw(); // re-draw the graph based on this new selection
-    });
 
-    // add in dropdown options from the unique values in the data
-    selectElement
-        .selectAll("option")
-        .data([
-            ...Array.from(new Set(state.taxes.map(d => d.Year))),
-            // default_selection,
-        ])
-        .join("option")
-        .attr("value", d => d)
-        .text(d => d);
     svg
         .selectAll(".state")
         // all of the features of the geojson, meaning all the states as individuals
         .data(state.geojson.features)
         .join("path")
         .attr("d", path)
+        .attr("stroke-linejoin", "round")
         .attr("class", "state")
-        .attr("fill", d => {
-            //console.log("d", d)
-            let value = newData.get(d.properties.NAME);
-            return (value != 0 ? colorScale(value) : "grey")
+        .attr("fill", "grey")
+        .attr('opacity', 0.5)
 
-        })
-        .style("stroke", "black")
-        .on('mouseover', (event, d) => {
-            //console.log("tooltip d", d)
-            div
-                .transition()
-                .duration(50)
-                .style('opacity', 1);
-            div
-                .html(
-                    "State: " + "<strong><h3>" + d.properties.NAME + "</strong></h3>" +
-                    "Average Cost per Pack: " + newDataYear.get(d.properties.NAME) +
-                    "<br>" + formatNumber(newData.get(d.properties.NAME))
+    draw();
 
-                )
-                .style("left", (event.pageX + 10) + "px")
-                .style("top", (event.pageY - 28) + "px");
-        })
-        .on('mouseout', () => {
-            div
-                .transition()
-                .duration(100)
-                .style('opacity', 0);
-        })
-
-    svg
-        .selectAll(".circle")
-        // all of the features of the geojson, meaning all the states as individuals
-        .data(state.taxes, d => d)
+    // called every times state is updated
+    svg.append("g")
+        .attr("fill", "brown")
+        .attr("fill-opacity", 0.5)
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 0.5)
+        .selectAll("circle")
+        .data(state.taxes
+            .map(d => d.Data_Value)
+            .sort((a, b) => b.Data_Value - a.Data_Value))
         .join("circle")
+        // .attr("transform", d => {
 
-        .attr("class", "circle")
-        .attr("fill", "#171730")
-        .attr('r', d => d.Data_Value)
-        .attr("opacity", 0.5)
-        .attr("transform", d => {
-            // d.features.map(e => { console.log(e); })
-            // console.log(d.features);
-            const [x, y] = projection([d.Longitude, d.Latitude]);
-            return `translate(${x}, ${y})`;
-        })
-
-    // // Run the code every 0.1 second
-    // d3.interval(function () {
-    //     // At the end of our data, loop back
-    //     time = (time < 214) ? time + 1 : 0
-    //     update(formattedData[time]);
-    // }, 100);
-
-    // // First run of the visualization
-    // update(formattedData[0]);
-
+        //     const [x, y] = projection([d.Longitude, d.Latitude]);
+        //     return `translate(${x}, ${y})`;
+        // })
+        .attr("transform", d => `translate(${path.centroid(state.geojson)})`)
+        .attr("r", d => d.Data_Value)
+        .append("title")
+        .text(d => `${d.LocationDesc}
+    ${d.Data_Value}`);
 }
-// called every times state is updated
-
 function draw() {
+    const t = svg.transition()
+        .duration(5000)
+        .ease(d3.easeLinear);
 
-    svg
-        .selectAll(".circle")
+    bubble = svg
+        //.append("g")
+        .selectAll("g")
         // all of the features of the geojson, meaning all the states as individuals
-        .data(state.taxes, d => d)
-        .join("circle")
+        .data(state.taxes, d => d.Id)
+        //.data(state.taxes.filter(d => d.Year === Year), d => d.Data_Value)
+        // .join("circle")
+        .join(enter =>
+            enter
+                .append("g")
+                .attr("transform", d => {
+                    // d.features.map(e => { console.log(e); })
+                    // console.log(d.features);
+                    const [x, y] = projection([d.Longitude, d.Latitude]);
+                    return `translate(${x}, ${y})`;
+                })
+                .call(g => g.append("circle")
+                    .attr("fill", "brown")
+                    .attr("class", "circle")
+                    .attr("opacity", 0.3)
+                    .attr("stroke", "currentColor")
+                    .attr('r', 0))
+                //d => d.Data_Value * 1.5)
+                //.attr("opacity", 0.5)
 
-        .attr("class", "circle")
-        .attr("fill", "blue")
-        .attr('r', d => d.Data_Value * 1.5)
-        .attr("opacity", 0.5)
-        .attr("transform", d => {
-            // d.features.map(e => { console.log(e); })
-            // console.log(d.features);
-            const [x, y] = projection([d.Longitude, d.Latitude]);
-            return `translate(${x}, ${y})`;
-        })
-        .on('mouseover', (event, d) => {
-            console.log("tooltip d", d)
-            div
-                .transition()
-                .duration(50)
-                .style('opacity', 1);
-            div
-                .html(
-                    "State: " + "<strong><h3>" + d.LocationDesc + "</strong></h3>" +
-                    "Average Cost per Pack: " + d.Data_Value
-                )
-                .style("left", (event.pageX + 10) + "px")
-                .style("top", (event.pageY - 28) + "px");
-        })
-        .on('mouseout', () => {
-            div
-                .transition()
-                .duration(100)
-                .style('opacity', 0);
-        })
+                .call(g => g.append("text")
+                    .attr("class", 'text')
+                    .attr("dy", "0.35em")
+                    .attr("fill-opacity", 1)
+                    .attr("y", -2)
+                    .text(d => d.LocationDesc)
+                    .attr("font-size", 10)
+                    .call(text => text.append("tspan")
+                        .style("font-variant-numeric", "tabular-nums")
+                        .attr("x", 0)
+                        .attr("y", 12)
+                        .text(0))
+                    .transition(t)
+                    .attr("opacity", 0.5))
+            ,
+            // .on('mouseover', (event, d) => {
+            //     console.log("tooltip d", d)
+            //     div
+            //         .transition()
+            //         .duration(50)
+            //         .style('opacity', 1);
+            //     div
+            //         .html(
+            //             "State: " + "<strong><h3>" + d.LocationDesc + "</strong></h3>" +
+            //             "Average Cost per Pack: " + formatNumber(d.Data_Value)
+            //         )
+            //         .style("left", (event.pageX + 10) + "px")
+            //         .style("top", (event.pageY - 28) + "px");
+            // })
+            // .on('mouseout', () => {
+            //     div
+            //         .transition()
+            //         .duration(100)
+            //         .style('opacity', 0);
+            // }),
+            update => update,
+            exit => exit.call(bubble => bubble.transition(t)
+                .call(g => g.select("circle").attr("r", 0))
+                .call(g => g.select("text").attr("fill-opacity", 0))
+                .call(g => g.select("tspan").call(textTween, 0))
+                .remove())
+            // exit => exit.call(exit => exit.transition()
+            //     .remove())
+        )
+    // .call(selection =>
+    //     selection.transition(d3.easeElastic)
+    //         .delay(d => d.Year + 1)
+    //         .attr("opacity", 0.3)
+    //         //.attr("fill", "blue")
+    //         .attr("r", d => d.Data_Value * 1.5))
+    bubble.select("circle").transition(t)
+        .attr("r", d => d.Data_Value * 1.5);
 
+    bubble.select("tspan").transition(t)
+        .call(textTween, d => d.Data_Value);
 };
---
+++ chart/gis_project/scatter_plot.js
@@ -0,0 +1,129 @@
+'use strict';
+d3.csv("../data/Average_cost_per_pack_LAT_LONG2.csv", d3.autoType).then(data => {
+
+    console.log(data);
+    let width = 360;
+    let height = 300;
+    let margin = { top: 60, bottom: 50, left: 60, right: 40 };
+    let tooltip;
+    let xScale;
+    let yScale;
+    let svg_scatter;
+    let div;
+
+    div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+    tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0)
+        .attr("position", "absolute");
+
+    xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.Year))
+        .range([margin.left, width - margin.right])
+
+    yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.Data_Value))
+        .range([height - margin.bottom, margin.top])
+
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"))
+
+    const yAxis = d3.axisLeft(yScale);
+    const formatNumber = d3.format(",.2f");
+
+    svg_scatter = d3
+        .select("#scatterplot-container")
+        .append("svg")
+        .attr("viewBox", "0 0 600 420")
+        .append("g")
+        .attr("transform", "translate(0,0)")
+
+    svg_scatter
+        .append("g")
+        .attr("class", "axis x-axis")
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(xAxis)
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("x", "30%")
+        .attr("dy", "3em")
+        .text("Year")
+        .attr("font-size", "9")
+        .attr("fill", "black")
+        .attr('opacity', 0.8)
+
+    svg_scatter
+        .append("g")
+        .attr("class", "axis y-axis")
+        .attr("transform", `translate(${margin.left}, 0)`)
+        .call(yAxis)
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("y", "50%") //in the middle of line
+        .attr("dx", "-3em")
+        .attr("writing-mode", "vertical-rl")
+        .text("Average cost of cigarettes per pack ($)")
+        .attr("font-size", "9")
+        .attr("fill", "black")
+        .attr('opacity', 0.8)
+
+
+    svg_scatter.append('g')
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(d.Year))
+        .attr("cy", d => yScale(d.Data_Value))
+        .attr("r", 2)
+        .attr("opacity", 0.2)
+        .attr("stroke", "black")
+        .style("fill", "black")
+
+        .on('mouseover', (event, d) => {
+            //console.log("tooltip d", d)
+            div
+                .transition()
+                .duration(50)
+                .style('opacity', 1);
+            div
+                .html(
+                    "State: " + "<strong><h3>" + d.LocationDesc + "</strong></h3>" +
+                    "Average Cost per Pack: " + formatNumber(d.Data_Value) +
+                    "<br>" + d.Year
+
+                )
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on('mouseout', () => {
+            div
+                .transition()
+                .duration(100)
+                .style('opacity', 0);
+        })
+
+        //     // Marks the ones already viewed
+        .on('mouseout', function (d) {
+            d3.select(this).transition()
+                .duration('50')
+                .attr('opacity', '1')
+                .attr('stroke', 'steelblue')
+
+            tooltip.transition()
+                .duration('50')
+                .style("opacity", 0);
+        })
+        .on('mouseover', function (d) {
+            d3.select(d.LocationDesc)
+                .transition()
+                .duration('50')
+                .attr('opacity', '1')
+                .attr('fill', 'steelblue')
+        })
+})
+
+
+    //const newData = new Map(state.taxes.map(d => [d.LocationDesc, d.Data_Value]))
+    //const newDataYear = new Map(state.taxes.map(d => [d.LocationDesc, d.Year]))
