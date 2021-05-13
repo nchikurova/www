cfe319bb863a2441bddedee052b20d21ab45306f
Nadezda Chikurova <nadezdachikurova@Nadezdas-MacBook-Air.local>
2021-05-13 14:08:06 -0400

    income added

--- chart/gis_project/main.js
+++ chart/gis_project/main.js
@@ -19,19 +19,17 @@ let projection2;
 let path2;
 let div2;
 
-let width3 = 460;
-let height3 = 400;
-let margin3 = { top: 60, bottom: 50, left: 40, right: 40 };
+let width3 = 500;
+let height3 = 300;
+let margin3 = { top: 20, bottom: 50, left: 40, right: 40 };
 
 let svg3;
-let projection3;
-let path3;
 let div3;
 
 let state = {
     geojson: null,
     taxes: null,
-    topten: null
+    income: null
 
 };
 
@@ -42,12 +40,16 @@ let state = {
 Promise.all([
     d3.json("../data/usState.json"),
     d3.csv("../data/Average_cost_per_pack_LAT_LONG.csv", d3.autoType),
-    //d3.csv("../data/top10_%_of_taxes.csv", d3.autoType)
-]).then(([geojson, taxes, topten]) => {
+    d3.csv("../data/income_2019.csv", d => ({
+        ...d,
+        Percentage: +d.Percentage,
+        Income: d.Income,
+    })),
+]).then(([geojson, taxes, income]) => {
     state.geojson = geojson;
     state.taxes = taxes;
-    state.topten = topten;
-    //console.log("state: ", state);
+    state.income = income;
+    console.log("state: ", state);
     init();
 });
 //const formatValue = d3.format(",d")
@@ -55,7 +57,6 @@ Promise.all([
 const formatNumber = d3.format(",.2f");
 
 function init() {
-
 
     projection = d3.geoAlbersUsa().fitSize([width, height], state.geojson);
     path = d3.geoPath().projection(projection);
@@ -80,7 +81,7 @@ function init() {
         .attr("class", "tooltip")
         .style("opacity", 0);
 
-    const dataYear = state.taxes.filter(d => d.Year === 2010)
+    const dataYear = state.taxes.filter(d => d.Year === 2015)
     //console.log("dataYear", dataYear)
     const newData = new Map(dataYear.map(d => [d.LocationDesc, d.Data_Value]))
     const newDataYear = new Map(dataYear.map(d => [d.LocationDesc, d.Year]))
@@ -139,7 +140,7 @@ function init() {
                 .style('opacity', 0);
         })
 
-
+    // SECOND MAP
     projection2 = d3.geoAlbersUsa().fitSize([width2, height2], state.geojson);
     path2 = d3.geoPath().projection(projection2);
 
@@ -169,21 +170,6 @@ function init() {
     const newDataYear2 = new Map(dataYear2.map(d => [d.LocationDesc, d.Year]))
 
     const colorScale2 = d3.scaleLinear().range(["whitesmoke", "black"]).domain([3, d3.max(state.taxes, d => d.Data_Value)])
-    //console.log(colorScale2.domain())
-    //const colorScale = d3.scaleOrdinal().range(["white", "whitesmoke", "grey", "black"]).domain([0, d3.max(state.taxes, d => d.Data_Value)])
-    //console.log(colorScale.domain())
-    // svg2
-    //     .selectAll(".state")
-    //     // all of the features of the geojson, meaning all the states as individuals
-    //     .data(state.geojson.features)
-    //     .join("path")
-    //     .attr("d", path2)
-    //     .attr("stroke-linejoin", "round")
-    //     .attr("class", "state")
-    //     .attr("fill", "grey")
-    //     .attr('opacity', 0.5)
-
-    // draw();
 
 
     svg2
@@ -224,87 +210,109 @@ function init() {
         })
 
     //BAR CHART
-    // const topFive = state.data.features.filter(d => d.properties.NUMPOINTS > 5000)
 
-    // topFiveData = new Map(topFive.map(d => [d.properties.ntaname, d.properties.NUMPOINTS]))
-    // console.log("topFiveData", topFiveData)
-    // svg3 = d3
-    //     .select("#d3-container2")
-    //     .append("svg")
-    //     .attr("width", width2)
-    //     .attr("height", height2);
+    svg3 = d3
+        .select("#bar-income")
+        .append("svg")
+        .attr("width", width3)
+        .attr("height", height3);
 
-    // div2 = d3.select("body").append("div")
-    //     .attr("class", "tooltip")
-    //     .style("opacity", 0)
+    div3 = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0)
 
-    // //console.log(topFiveData)
-    // const xScale = d3
-    //     .scaleBand()
-    //     .domain(topFiveData, d => d.key)
-    //     .range([margin2.left, width2 - margin2.right])
-    //     .paddingInner(0.5);
+    const yScale = d3
+        .scaleBand()
+        .domain(state.income.map(d => d['Income']))
+        .range([margin3.top, height3 - margin3.bottom])
+        .paddingInner(0.2)
+        .paddingOuter(0.2)
+
+    const xScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(state.income, d => d.Percentage)])
+        .range([margin3.left + 5, width3 - margin3.right]);
+
+    console.log("x", xScale.domain(), "y", yScale.domain())
+    // reference for d3.axis: https://github.com/d3/d3-axis
+    const xAxis = d3.axisBottom(xScale).ticks(state.income.length)
+    const yAxis = d3.axisLeft(yScale).tickValues([])
+
+    // reference for d3.axis: https://github.com/d3/d3-axis
+
+    svg3
+        .selectAll("rect")
+        .data(state.income)
+        .join("rect")
+        .attr("class", "rect")
+        .attr("x", margin3.left + 5)//d => xScale(d.Percentage))
+        .attr("y", d => yScale(d.Income))
+        .attr("height", yScale.bandwidth())
+        .attr("width", d => xScale(d.Percentage))
+        .attr("fill", "grey")
+        .on('mouseover', (event, d) => {
+            // console.log("tooltip d", d)
+            div3
+                .transition()
+                .duration(50)
+                .style('opacity', 1);
+            div3
+                .html("Income: " + "<h3><strong>" + d.Income + "</strong></h3>" +
+                    "Percentage: " + "<strong>" + d.Percentage + "%" + "</strong>"
+                )
+
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on('mouseout', () => {
+            div3
+                .transition()
+                .duration(100)
+                .style('opacity', 0);
+        })
+    svg3
+        .selectAll("text")
+        .data(state.income)
+        .join("text")
+        .attr("class", "label_bar")
+        // this allows us to position the text in the center of the bar
+        .attr("y", d => yScale(d.Income))
+        .attr("x", d => xScale(d.Percentage) + 35)
+        .text(d => d.Income)
+        .attr("dy", "1.6em")
 
 
-    // const yScale = d3
-    //     .scaleLinear()
-    //     .domain([0, 18606])
-    //     .range([height2 - margin2.bottom, margin2.top]);
+    svg3
+        .append("g")
+        .attr("class", "axis x-axis")
 
-    // console.log("x", xScale.domain(), "y", yScale.domain())
-    // // reference for d3.axis: https://github.com/d3/d3-axis
-    // const xAxis = d3.axisBottom(xScale)//.ticks(state.data.length);
+        .attr("transform", `translate(0,${height3 - margin3.bottom})`)
+        .call(xAxis)
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("x", "15%")
+        .attr("dy", "-19em")
+        .text("Percentage")
+        .attr("font-size", "14")
+        .attr("fill", "black")
+        .attr('opacity', 0.8)
+
+    svg3
+        .append("g")
+        .attr("class", "axis y-axis")
+        .attr("transform", `translate(${margin3.left}, ${margin3.right})`)
+        .call(yAxis)
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("y", "15%") //in the middle of line
+        .attr("dx", "-1em")
+        .attr("writing-mode", "vertical-rl")
+        .text("Income")
+        .attr("font-size", "14")
+        .attr("fill", "black")
+        .attr('opacity', 0.8)
 
 
-    // // reference for d3.axis: https://github.com/d3/d3-axis
-
-    // svg2
-    //     .selectAll("rect")
-    //     .data(topFiveData)
-    //     .join("rect")
-    //     .attr("y", d => yScale(d.value))
-    //     .attr("x", d => xScale(d.key))
-    //     .attr("width", xScale.bandwidth())
-    //     .attr("height", d => yScale(d.value))//height2 - yScale(d.value))
-    //     .attr("fill", "steelblue")
-    //     .on('mouseover', (event, d) => {
-    //         // console.log("tooltip d", d)
-    //         div2
-    //             .transition()
-    //             .duration(50)
-    //             .style('opacity', 1);
-    //         div2
-    //             .html("Neighborhood name: " + "<h3><strong>" + d.key + "</strong></h3>" +
-    //                 "Number of Noise Related Complaints in 2020: " + "<strong>" + d.value + "</strong>"
-    //             )
-
-    //             .style("left", (event.pageX + 10) + "px")
-    //             .style("top", (event.pageY - 28) + "px");
-    //     })
-    //     .on('mouseout', () => {
-    //         div2
-    //             .transition()
-    //             .duration(100)
-    //             .style('opacity', 0);
-    //     })
-
-    // // append text
-    // const text2 = svg2
-    //     .selectAll("text")
-    //     .data(topFiveData)
-    //     .join("text")
-    //     .attr("class", "label")
-    //     // this allows us to position the text in the center of the bar
-    //     .attr("x", d => xScale(d.key))// + (xScale.bandwidth() / 2))
-    //     .attr("y", d => yScale(d.value))
-    //     .text(d => d.value)
-    //     .attr("dy", "1.25em");
-
-    // svg2
-    //     .append("g")
-    //     .attr("class", "axis")
-    //     .attr("transform", `translate(0, ${height2 - margin2.bottom})`)
-    //     .call(xAxis)
 }
 function draw() {
 
