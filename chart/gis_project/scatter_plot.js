4a4a643394bb3d55fa7194eee052835b63a3e254
Nadezda Chikurova <nadezdachikurova@Nadezdas-MacBook-Air.local>
2021-05-16 13:15:35 -0400

    reorganized

--- chart/gis_project/scatter_plot.js
+++ chart/gis_project/scatter_plot.js
@@ -2,14 +2,21 @@
 d3.csv("../data/Average_cost_per_pack_LAT_LONG2.csv", d3.autoType).then(data => {
 
     //console.log(data);
-    let width = 360;
-    let height = 300;
+    let width = 500;
+    let height = 400;
     let margin = { top: 60, bottom: 50, left: 60, right: 40 };
     let tooltip;
     let xScale;
     let yScale;
     let svg_scatter;
     let div;
+    let width_scatter = 460;
+    let height_scatter = 400;
+    let margin_scatter = { top: 60, bottom: 50, left: 150, right: 40 };
+    let xScale_scatter;
+    let yScale_scatter;
+    let svg_scatter_bar;
+    let div_scatter;
 
     div = d3.select("body").append("div")
         .attr("class", "tooltip")
@@ -35,36 +42,38 @@ d3.csv("../data/Average_cost_per_pack_LAT_LONG2.csv", d3.autoType).then(data => 
     svg_scatter = d3
         .select("#scatterplot-container")
         .append("svg")
-        .attr("viewBox", "0 0 600 420")
-        .append("g")
-        .attr("transform", "translate(0,0)")
+        .attr("width", width)
+        .attr("height", height);
+    // .attr("viewBox", "0 0 600 420")
+    // .append("g")
+    // .attr("transform", "translate(0,0)")
 
     svg_scatter
         .append("g")
-        .attr("class", "axis x-axis")
+        .attr("class", "axis x-axis-scatter")
         .attr("transform", `translate(0,${height - margin.bottom})`)
         .call(xAxis)
         .append("text")
-        .attr("class", "axis-label")
-        .attr("x", "30%")
+        .attr("class", "axis-label-scatter")
+        .attr("x", "50%")
         .attr("dy", "3em")
         .text("Year")
-        .attr("font-size", "9")
+        .attr("font-size", "14")
         .attr("fill", "black")
-        .attr('opacity', 0.8)
+        .attr('opacity', 0.9)
 
     svg_scatter
         .append("g")
-        .attr("class", "axis y-axis")
+        .attr("class", "axis y-axis-scatter")
         .attr("transform", `translate(${margin.left}, 0)`)
         .call(yAxis)
         .append("text")
-        .attr("class", "axis-label")
-        .attr("y", "50%") //in the middle of line
+        .attr("class", "axis-label-scatter")
+        .attr("y", "80%") //in the middle of line
         .attr("dx", "-3em")
         .attr("writing-mode", "vertical-rl")
         .text("Average cost of cigarettes per pack ($)")
-        .attr("font-size", "9")
+        .attr("font-size", "14")
         .attr("fill", "black")
         .attr('opacity', 0.8)
 
@@ -76,7 +85,7 @@ d3.csv("../data/Average_cost_per_pack_LAT_LONG2.csv", d3.autoType).then(data => 
         .append("circle")
         .attr("cx", d => xScale(d.Year))
         .attr("cy", d => yScale(d.Data_Value))
-        .attr("r", 2)
+        .attr("r", 4)
         .attr("opacity", 0.2)
         .attr("stroke", "black")
         .style("fill", "black")
@@ -110,20 +119,127 @@ d3.csv("../data/Average_cost_per_pack_LAT_LONG2.csv", d3.autoType).then(data => 
                 .duration('50')
                 .attr('opacity', '1')
                 .attr('stroke', 'steelblue')
+                .attr("fill", 'steelblue')
 
             tooltip.transition()
                 .duration('50')
                 .style("opacity", 0);
         })
-    // .on('mouseover', function (d) {
-    //     d3.select(d.LocationDesc)
-    //         .transition()
-    //         .duration('50')
-    //         .attr('opacity', '1')
-    //         .attr('fill', 'steelblue')
-    // })
+    // BAR CHART TOP 10
+
+    const scatterData = data.filter(d => d.Year === 2019)
+    const topTen = scatterData.filter(d => d.Data_Value > 8.6)
+    console.log("scatterData", scatterData, topTen)
+
+    svg_scatter_bar = d3
+        .select("#scatter-bar")
+        .append("svg")
+        .attr("width", width_scatter)
+        .attr("height", height_scatter);
+
+    div_scatter = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+
+    yScale_scatter = d3
+        .scaleBand()
+        .domain(["New York", "District of Columbia", "Connecticut", "Rhode Island", "Massachusetts", "Alaska", "Hawaii", "Minnesota", "Illinois", "Vermont"])
+
+        // .domain(topTen, d => d['LocationDesc'])
+        .range([margin_scatter.top, height_scatter - margin_scatter.bottom])
+        .paddingInner(0.2)
+        .paddingOuter(0.2);
+
+
+    xScale_scatter = d3
+        .scaleLinear()
+        .domain([0, d3.max(topTen, d => d.Data_Value)])//58])
+        .range([margin_scatter.left, width_scatter - margin_scatter.right]);
+
+    //console.log("x_bar", xScale_bar.domain(), "y_bar", yScale_bar.domain())
+    // reference for d3.axis: https://github.com/d3/d3-axis
+    const xAxis_scatter = d3.axisBottom(xScale_scatter)//.ticks(data_bar.length)
+    const yAxis_scatter = d3.axisLeft(yScale_scatter)//.tickValues([])
+
+    svg_scatter_bar
+        .selectAll("rect")
+        .data(topTen)
+        .join("rect")
+        .attr("class", "rect.scatter")
+        .attr("y", d => yScale_scatter(d.LocationDesc))
+        .attr("x", margin_scatter.left)//d => xScale_bar(d))
+        .attr("height", yScale_scatter.bandwidth())
+        .attr("width", d => xScale_scatter(d.Data_Value) - margin_scatter.left)//- margin_scatter.right)
+        .attr("fill", " rgb(190, 184, 192)")
+
+        .on('mouseover', (event, d) => {
+            // console.log("tooltip d", d)
+            div_scatter
+                .transition()
+                .duration(50)
+                .style('opacity', 1);
+            div_scatter
+                .html("State: " + "<h3><strong>" + d.LocationDesc + "</strong></h3>" +
+                    "Average Cost " + "<strong>" + formatNumber(d.Data_Value) + "</strong>"
+                )
+
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on('mouseout', () => {
+            div_scatter
+                .transition()
+                .duration(100)
+                .style('opacity', 0);
+        })
+
+    //append text
+    const text1 = svg_scatter_bar
+        .selectAll("text")
+        .data(topTen)
+        .join("text")
+        .attr("class", "label_bar_scatter")// this allows us to position the text in the center of the bar
+        .attr("x", margin_scatter.left + 20)//d => xScale_scatter(d.Data_Value))// + (xScale.bandwidth() / 2))
+        .attr("y", d => yScale_scatter(d.LocationDesc))
+        .text(d => `${formatNumber(d.Data_Value) + "$"}`)
+        .attr("dy", "1em");
+
+
+    svg_scatter_bar
+        .append("g")
+        .attr("class", "axis x-axis-line")
+        .attr("transform", `translate(0, ${height_scatter - margin_scatter.bottom})`)
+        //.attr("transform", `translate(0, ${ height3 - margin3.bottom})`)
+        .call(xAxis_scatter)
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("x", "50%")
+        .attr("dy", "2.4em")
+        .text("Average Cost Per Pack, $")
+        .attr("font-size", "16")
+        .attr("fill", "black")
+        .attr('opacity', 0.8)
+
+    svg_scatter_bar
+        .append("g")
+        .attr("class", "axis y-axis-line")
+        .attr("transform", `translate(${margin_scatter.left}, 0)`)
+
+        .call(yAxis_scatter)
+        .append("text")
+        .attr("class", "axis-label")
+    // .attr("y", "40%") //in the middle of line
+    // .attr("dx", "-0.7em")
+    // .attr("writing-mode", "vertical-rl")
+    // .text("State")
+    // .attr("font-size", "16")
+    // .attr("fill", "black")
+    // .attr('opacity', 0.8)
+
+
+
 })
 
 
-    //const newData = new Map(state.taxes.map(d => [d.LocationDesc, d.Data_Value]))
-    //const newDataYear = new Map(state.taxes.map(d => [d.LocationDesc, d.Year]))
+
